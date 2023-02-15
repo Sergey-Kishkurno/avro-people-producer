5b5e862e697ee2fd43deb02b4615829e88892685
Sergey Kishkurno <s.kishkurno@gmail.com>
2023-02-15 22:13:55 +0200

    add topic configs

---
+++ src/main/java/com/sample/avropeopleproducer/configs/KafkaConfig.java
@@ -0,0 +1,27 @@
+package com.sample.avropeopleproducer.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Value("${topics.people-avro.name}")
+    String peopleAvroTopic;
+
+    @Value("${topics.people-avro.replicas}")
+    private int peopleAvroTopicReplicas;
+
+    @Value("${topics.people-avro.partitions}")
+    private int peopleAvroTopicPartitions;
+
+    @Bean
+    public NewTopic makePeopleAvroTopic() {
+        return TopicBuilder.name(peopleAvroTopic)
+                .replicas(peopleAvroTopicReplicas)
+                .partitions(peopleAvroTopicPartitions)
+                .build();
+    }
+}
