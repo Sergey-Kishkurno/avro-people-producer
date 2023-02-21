d3159056b319b23c5cf1498e9a7d92fcfdbe6276
Sergey Kishkurno <s.kishkurno@gmail.com>
2023-02-22 00:30:09 +0200

    add controller

---
+++ src/main/java/com/sample/avropeopleproducer/controllers/PeopleController.java
@@ -0,0 +1,10 @@
+package com.sample.avropeopleproducer.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PeopleController {
+
+}
