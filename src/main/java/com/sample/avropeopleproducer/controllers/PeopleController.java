4d8cd2433ec5ffece53adcedd9558d53e556bd84
Sergey Kishkurno <s.kishkurno@gmail.com>
2023-02-24 00:02:36 +0200

    add controller

--- src/main/java/com/sample/avropeopleproducer/controllers/PeopleController.java
+++ src/main/java/com/sample/avropeopleproducer/controllers/PeopleController.java
@@ -1,10 +1,58 @@
 package com.sample.avropeopleproducer.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.javafaker.Faker;
+import com.sample.avropeopleproducer.commands.CreatePeopleCommand;
+import com.sample.avropeopleproducer.configs.KafkaConfig;
+import com.sample.avropeopleproducer.models.PersonDto;
+import kafka.sr.sample.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class PeopleController {
+    static final Logger logger = LoggerFactory.getLogger(PeopleController.class);
+
+    @Value("${topics.people-avro.name}")
+    private String personAvroTopic;
+
+    private final KafkaTemplate<String, Person> kafkaTemplate;
+
+    public PeopleController(KafkaTemplate<String, Person> kafkaTemplate) {this.kafkaTemplate = kafkaTemplate;};
+
+    @PostMapping("/people")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<PersonDto> create(@RequestBody CreatePeopleCommand cmd) {
+        logger.info("Creating people cmd " + cmd);
+        List<PersonDto> people = new ArrayList<>();
+        var faker = new Faker();
+        for (var i=0; i < cmd.getCount(); i++) {
+            var person = new Person();
+            person.setName(faker.name().fullName());
+            person.setTitle(faker.job().title());
+            people.add(new PersonDto(person.getName(), person.getTitle()));
+
+            var future = kafkaTemplate.send(
+                    personAvroTopic,
+                    person.getName().toLowerCase().replaceAll("\\s+", "-"),
+                    person
+            );
+
+            future.addCallback(
+                    result -> {},
+                    ex -> {}
+            );
+        }
+        kafkaTemplate.flush();
+
+        return people;
+    }
 
 }
