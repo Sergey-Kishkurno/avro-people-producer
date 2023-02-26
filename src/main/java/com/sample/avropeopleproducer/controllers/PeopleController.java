bd3ac561671ef66ee8845911f28901b109840aaa
Sergey Kishkurno <s.kishkurno@gmail.com>
2023-02-26 19:35:52 +0200

    comments and cleanup

--- src/main/java/com/sample/avropeopleproducer/controllers/PeopleController.java
+++ src/main/java/com/sample/avropeopleproducer/controllers/PeopleController.java
@@ -63,6 +63,7 @@ public class PeopleController {
 //                    ex -> { logger.error("Failed  to produce: {}", person, ex); }
 //            );
         }
+
         kafkaTemplate.flush();
 
         return people;
